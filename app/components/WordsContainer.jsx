81b0e6b0372ed102466eb8dccadd860254dddcdb
Kris-Furer <krisfurer@gmail.com>
2024-07-05 18:51:35 +0200

    Added saved words

--- app/components/WordsContainer.jsx
+++ app/components/WordsContainer.jsx
@@ -5,7 +5,11 @@ import GenerateButton from "./GenerateButton";
 
   const WordsContainer = () => {
    
-    const [words, setWords] = useState([]);
+    const [words, setWords] = useState([])
+    const [savedWords, setSavedWords] = useState(["Your Saved Goes Here"])
+
+   const fakeData = ["cow", "pig"]
+
   
     // Fetch the words from the api and set 'words' variable to the result
     const fetchWords = async () => {
@@ -19,6 +23,11 @@ import GenerateButton from "./GenerateButton";
       }
     };
 
+    // Add the new saved word to the previous
+    const saveWord = (word) => {
+      setSavedWords([...savedWords, word]);
+    }
+
 
   return (
     <div>
@@ -26,7 +35,14 @@ import GenerateButton from "./GenerateButton";
       <p className='m-6'>Here are the words</p>
       <ul className='m-6'>
         {words.map((word) => (
-          <li className='p-6 border border-white m-3 inline-block' key={word}>{word}</li>
+          // Within arrow function to prevent excecuting on render
+          <li onClick={() => saveWord(word)} className='p-6 border border-white m-3 inline-block' key={word}>{word}</li>
+          ))}
+      </ul>
+      <p>Saved Words</p>
+      <ul className='m-6'>
+        {savedWords.map((savedWord) => (
+          <li className='p-6 border border-white m-3 inline-block' key={savedWord}>{savedWord}</li>
           ))}
       </ul>
     </div>
